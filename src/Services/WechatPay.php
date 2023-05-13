218aa055acdff16c585e03ef51e69301a2b95fb4
571816409@qq.com <571816409@qq.com>
2023-05-13 16:56:57 +0800

    初始化

---
+++ src/Services/WechatPay.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Www\WxPay\Services;
+
+use Curl\StrService\StrService;
+use WeChatPay\Builder;
+use WeChatPay\Crypto\Rsa;
+use WeChatPay\Util\PemUtil;
+use QrCode;
+use function App\Services\env;
+
+class WechatPay
+{
+
+    private $chain = '';
+    private $appId = '';
+
+    public function __construct($appId = null,$chain = null)
+    {
+        $this->chain = $chain ? : config('wx_pay.chain');
+        $this->appId = $appId  ? : config('wx_pay.app_Id');
+    }
+
+    /**
+     * 支付调起
+     * @param $orderNo
+     * @param $des
+     * @return mixed|void
+     */
+    public function pay($orderNo,$des){
+
+        $instance = $this->payBuildInstance();
+
+        try {
+            $resp = $instance
+                ->chain($this->chain)
+                ->post(['json' => [
+                    'mchid'        => config('wx_pay.mch_id'),
+                    'out_trade_no' => $orderNo,
+                    'appid'        => $this->appId,
+                    'description'  => $des,
+                    'notify_url'   => config('wx_pay.notify_url'),
+                    'amount'       => [
+                        'total'    => rand(1,100),
+                        'currency' => 'CNY'
+                    ],
+                ]]);
+
+            //echo $resp->getStatusCode(), PHP_EOL;
+             return json_decode($resp->getBody(),true);
+            //header('location: '.$redireurl['code_url']);
+        } catch (\Exception $e) {
+            // 进行错误处理
+            echo $e->getMessage(), PHP_EOL;
+            if ($e instanceof \GuzzleHttp\Exception\RequestException && $e->hasResponse()) {
+                $r = $e->getResponse();
+                echo $r->getStatusCode() . ' ' . $r->getReasonPhrase(), PHP_EOL;
+                echo $r->getBody(), PHP_EOL, PHP_EOL, PHP_EOL;
+            }
+            echo $e->getTraceAsString(), PHP_EOL;
+        }
+
+    }
+
+    /**
+     * 创建支付实例
+     * @return \WeChatPay\BuilderChainable
+     */
+    private function payBuildInstance(){
+
+
+        // 商户号
+        $merchantId = config('wx_pay.mch_id');
+
+        // 从本地文件中加载「商户API私钥」，「商户API私钥」会用来生成请求的签名
+        $merchantPrivateKeyFilePath = config('wx_pay.apiclient_key');
+        $merchantPrivateKeyInstance = Rsa::from($merchantPrivateKeyFilePath, Rsa::KEY_TYPE_PRIVATE);
+
+        // 「商户API证书」的「证书序列号」
+        $merchantCertificateSerial = config('wx_pay.merchant_certificate_serial');
+
+        // 从本地文件中加载「微信支付平台证书」，用来验证微信支付应答的签名
+        $platformCertificateFilePath = config('wx_pay.platform_secret.pem');;
+        $platformPublicKeyInstance = Rsa::from($platformCertificateFilePath, Rsa::KEY_TYPE_PUBLIC);
+
+        // 从「微信支付平台证书」中获取「证书序列号」
+        $platformCertificateSerial = PemUtil::parseCertificateSerialNo($platformCertificateFilePath);
+
+        // 构造一个 APIv3 客户端实例
+        $instance = Builder::factory([
+            'mchid'      => $merchantId,
+            'serial'     => $merchantCertificateSerial,
+            'privateKey' => $merchantPrivateKeyInstance,
+            'certs'      => [
+                $platformCertificateSerial => $platformPublicKeyInstance,
+            ],
+        ]);
+
+        return $instance;
+    }
+}
--- src/config/wx_pay.php
+++ src/config/wx_pay.php
@@ -3,9 +3,11 @@
 return[
     'api_prefix'        => 'api',                                    //路由前缀
     'mch_id'            => env('MCH_ID'),                            //商户号ID
-    'apply_app_id'      => env('APPLY_APP_ID'),                      //小程序APP_ID
-    'apply_secret'      => env('APPLY_SECRET'),                      //小程序SECRET
-    'send_name'         => env('SEND_NAME'),                         //商家名称
+    'merchant_certificate_serial' => env('MET_NO'),                  //商户序列号
+    'apiclient_key'     => env('API_CLIENT_KEY'),                    //支付密钥KEY
+    'platform_secret'   => env('PLATFORM_SECRET'),                   //支付平台密钥，工具下载得来
+    'chain'             => env('CHAIN'),                             //调起微信支付聚到
+    'app_id'            => env('APP_ID'),                            //支付的APPId  如小程序，公众号,移动端，H5
     'code' => [
         'success'   => [0,'成功'],
         'fail'      => [4001,'失败']
