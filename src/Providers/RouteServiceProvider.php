2c6bc0baad2ab7c57663afc335089f29f257d923
571816409@qq.com <571816409@qq.com>
2023-05-13 15:30:55 +0800

    初始化

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace WxLogin\Providers;
+namespace Www\WxPay\Providers;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
